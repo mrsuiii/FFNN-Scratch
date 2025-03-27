9edc43539f0b7dc86731b89f4285836d86618d40
mrsuiii <emeryfathanzwageri@gmail.com>
2025-03-27 23:16:59 +0700

    add rmsnorm

--- Layer.py
+++ Layer.py
@@ -1,24 +1,38 @@
 from activation import linear
-from init import he_init
+from init import he_init 
 import numpy as np
 from Value import Value
 from typing import List, Optional, Dict, Callable
-from loss import bce_loss
 
 class Layer:
     def __init__(self, n_inputs: int, n_neurons: int, 
-                 activation: Callable[[Value], Value] = linear, 
+                 activation: Callable[[Value], Value] = linear,
                  weight_init: Callable[..., np.ndarray] = lambda n_in, n_out: np.zeros((n_out, n_in)),
-                 weight_init_kwargs: Optional[Dict] = None):
+                 weight_init_kwargs: Optional[Dict] = None,
+                 rmsnorm: bool = False,
+                 rmsnorm_epsilon: float = 1e-8):
         if weight_init_kwargs is None:
             weight_init_kwargs = {}
 
         self.activation = activation
         self.W = Value(data=weight_init(n_inputs, n_neurons, **weight_init_kwargs))
         self.b = Value(data=np.zeros(n_neurons))
-
+        self.rmsnorm = rmsnorm
+        self.rmsnorm_epsilon = rmsnorm_epsilon
+        if self.rmsnorm:
+            self.gamma = Value(np.random.randn((1, n_neurons)))  
+        else:
+            self.gamma = None
     def parameters(self) -> List[Value]:
         return [self.W, self.b]
-    
+
+    def rmsnorm(self, x: Value) -> Value:
+        ms = Value(np.mean(x.data ** 2, axis=1, keepdims=True))
+        rms = Value(np.sqrt(ms.data + self.rmsnorm_epsilon))
+        return x / rms
+
     def __call__(self, x: Value) -> Value:
-        return self.activation(x.matmul(self.W.T) + self.b)
+        z = x.matmul(self.W.T) + self.b
+        if self.rmsnorm:
+            z = self.rmsnorm(z)
+        return self.activation(z)
